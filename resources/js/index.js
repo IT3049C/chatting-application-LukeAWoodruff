71d168a953432aff0290098a6deb885740316df6
LukeAWoodruff <woodruffluke0@gmail.com>
2025-02-10 20:02:01 -0500

    created references to the html in index.js

---
+++ resources/js/index.js
@@ -0,0 +1,4 @@
+const nameInput = document.getElementById("Luke Woodruff");
+const myMessage = document.getElementById("my-message");
+const chatbox =  document.getElementById("chat");
+const sendButton = document.getElementById("send-button");
