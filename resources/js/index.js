0b7e645485980b6cf4526fdca616a7a4b2ee249c
LukeAWoodruff <woodruffluke0@gmail.com>
2025-02-11 01:14:57 -0500

    fixed my-message into my-message-input

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,5 @@
 const nameInput = document.getElementById("Luke-Woodruff");
-const myMessage = document.getElementById("my-message");
+const myMessage = document.getElementById("my-message-input");
 const chatbox =  document.getElementById("chat");
 const sendButton = document.getElementById("send-button");
 
