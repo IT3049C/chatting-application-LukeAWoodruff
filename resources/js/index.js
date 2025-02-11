10b398bf4c5883898136dfa9e3dd013a913b57dc
LukeAWoodruff <woodruffluke0@gmail.com>
2025-02-10 21:03:31 -0500

    Created a function that takes a message object as a function argument and returns html

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,3 +2,28 @@ const nameInput = document.getElementById("Luke Woodruff");
 const myMessage = document.getElementById("my-message");
 const chatbox =  document.getElementById("chat");
 const sendButton = document.getElementById("send-button");
+function formatMessage(message, myNameInput){
+const time = newDate(message.timestamp);
+const formattedTime = `${time.getHours()}:${time.getMinutes()}`;
+if (myNameInput === message.sender){
+    return`
+    <div class = "mine messages">
+    <div class = "message">
+    ${message.text}
+    </div>
+    </div>`;
+}else{
+    return`
+    <div class = "your messages">
+    <div class = "message">
+    ${message.text}
+    </div>
+    </div>`;
+    }
+} 
+{
+    "id"; 1,
+    "text"; "This is my message",
+    "sender"; "Yahya Gilany",
+    "timestamp"; 1537410673072
+  }
